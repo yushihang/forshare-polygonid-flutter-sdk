858f70c3b1deddce9712449fc2e7947926795d95
yushihang <yushihang@gmail.com>
2023-11-16 15:26:37 +0800

    add fat lib for ios simulator

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:polygonid_flutter_sdk/sdk/polygon_id_sdk.dart';
+import 'package:polygonid_flutter_sdk_example/src/presentation/app.dart';
 import 'package:polygonid_flutter_sdk_example/src/presentation/dependency_injection/dependencies_provider.dart'
     as di;
-import 'package:polygonid_flutter_sdk_example/src/presentation/app.dart';
 
 Future<void> main() async {
   //Dependency Injection initialization
