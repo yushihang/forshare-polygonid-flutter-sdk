fdebf1df3493f7d730b9985bc5b37176dc104018
yushihang <shihang.yu@hsbc.com>
2023-12-27 16:30:07 +0800

    read and set system proxy

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:native_flutter_proxy/custom_proxy.dart';
+import 'package:native_flutter_proxy/native_proxy_reader.dart';
 import 'package:polygonid_flutter_sdk/identity/data/data_sources/lib_babyjubjub_data_source.dart';
 import 'package:polygonid_flutter_sdk/identity/libs/bjj/bjj.dart';
 import 'package:polygonid_flutter_sdk/sdk/polygon_id_sdk.dart';
@@ -9,6 +11,23 @@ import 'package:polygonid_flutter_sdk_example/src/presentation/dependency_inject
 
 Future<void> main() async {
   //Dependency Injection initialization
+  WidgetsFlutterBinding.ensureInitialized();
+  bool enabled = false;
+  String? host;
+  int? port;
+  try {
+    ProxySetting settings = await NativeProxyReader.proxySetting;
+    enabled = settings.enabled;
+    host = settings.host;
+    port = settings.port;
+  } catch (e) {
+    print(e);
+  }
+  if (enabled && host != null) {
+    final proxy = CustomProxy(ipAddress: host, port: port);
+    proxy.enable();
+    print("proxy enabled");
+  }
 
   final bjj = LibBabyJubJubDataSource(BabyjubjubLib());
   final result = await bjj.hashPoseidon("1");
