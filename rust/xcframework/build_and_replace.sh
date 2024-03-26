4e5771f87973745818382491c266e2e8dff168eb
yushihang <yushihang@gmail.com>
2024-03-26 14:13:29 +0800

    Update build_and_replace.sh

--- rust/xcframework/build_and_replace.sh
+++ rust/xcframework/build_and_replace.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#rustup toolchain install nightly-aarch64-apple-darwin
 #rustup component add rust-src --toolchain nightly-aarch64-apple-darwin
 #rustup target add aarch64-apple-ios-sim --toolchain nightly
 #rustup target add aarch64-apple-ios x86_64-apple-ios aarch64-apple-ios-sim
